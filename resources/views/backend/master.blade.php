d7dbe593b604764476d66c40b4da05500a16de12
TongmengHAI <tongmeng016@gmail.com>
2024-06-27 19:26:53 +0700

    fix bug

--- resources/views/backend/master.blade.php
+++ resources/views/backend/master.blade.php
@@ -68,7 +68,7 @@
                 <ul class="menu-inner py-1">
                     <!-- Dashboard -->
                     <li class="menu-item">
-                        <a href="/admin" class="menu-link">
+                        <a href="/dashboard" class="menu-link">
                             <i class="menu-icon tf-icons bx bx-home-circle"></i>
                             <div data-i18n="Analytics">Dashboard</div>
                         </a>
@@ -138,7 +138,7 @@
                         </ul>
                     </li>
 
-                    
+
 
                     <li class="menu-item">
                         <a href="javascript:void(0);" class="menu-link menu-toggle">
